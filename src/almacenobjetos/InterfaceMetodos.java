6bd6580ea50acf5b84c1198bcb78bd668c315025
minimerlyn <gabrielrv98@gmail.com>
2017-09-27 01:00:52 +0200

    añadaida interface
    eso

---
+++ src/almacenobjetos/InterfaceMetodos.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package almacenobjetos;
+
+/**
+ *
+ * @author reyga
+ */
+public interface InterfaceMetodos  {
+    
+    String busca(String searching);
+    
+}
